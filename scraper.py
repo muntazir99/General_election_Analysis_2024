1f1286d827e346875fdf0c2e7711bd49336edfe7
Muntazir Jahangir <muntazir9934@gmail.com>
2024-06-29 10:14:30 +0530

    scraping-csv

--- scraper.py
+++ scraper.py
@@ -1,4 +1,7 @@
 import requests
+from bs4 import BeautifulSoup
+import csv
+import os
 
 base_url = "https://results.eci.gov.in/PcResultGenJune2024/Constituencywise"
 
@@ -13,3 +16,26 @@ for state in states:
         print("URL", url)
         response = requests.get(url)
         print("Response", response)
+        if response.status_code == 404:
+            break
+        
+        response_text = response.text
+        soup = BeautifulSoup(response_text, 'html.parser')
+
+        table = soup.find('table')  # Find the table element
+
+        # Extract the table headers
+        headers = [th.text.strip() for th in table.find_all('th')]
+
+        # Extract the table data
+        data = []
+        for tr in table.find_all('tr'):
+            row = [td.text.strip() for td in tr.find_all('td')]
+            if row:
+                data.append(row)
+
+        # Write data to CSV file
+        with open('output.csv', 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(headers)
+            writer.writerows(data)
